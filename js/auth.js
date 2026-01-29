58b6cb94a44725d5b6452b17f595078aec61be1c
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-01-29 12:02:24 -0300

    Página de login Implementada
    Foi adicionado nesta página de login também um cachê para evitar fetchs repetidos a cada requisição ao json.

--- js/auth.js
+++ js/auth.js
@@ -1,19 +1,24 @@
 const submit = document.querySelector("#submit")
 const inputs = document.querySelectorAll(".input-login")
-
-submit.addEventListener('click', () => {
-    getData()
-})
+const loading = document.querySelector("#container-loading")
+let cached_data = null;
 
 async function getData(){
+    if(cached_data){
+        auth(cached_data)
+        return;
+    }
+
     const url = '../data.json'
     try {
         const response = await fetch(url)
+        
         if(!response.ok){
             throw new Error(`Response status: ${response.status}`)
         }
-        
+
         const result = await response.json()
+        cached_data = result
         auth(result)
     } catch (error) {
         console.log(error.message)
@@ -21,31 +26,47 @@ async function getData(){
 }
 
 const auth = (result) => {
-    result.users.forEach(element => {
-        const u = inputs[0].value
-        const p = inputs[1].value
-        
-        if(u === '' && p === ''){
-            console.log("Usuário inválido.")
-        }
-        else if(u === element.u && p === ''){
-            console.log("Senha não informada.")
-        }
-        else if(u != '' && p != ''){
-            const loading = document.querySelector("#container-loading")
-            const isloading = loading.getAttribute('data-isloading')
-        
-            if(isloading === 'false'){
-                loading.setAttribute('data-isloading', 'true')
-            }   
+    const u = inputs[0].value
+    const p = inputs[1].value
+    const message = document.querySelector("#message")
 
-            if(u != element.u || p != element.p){
-                console.log("Nome de usuário ou senha incorretos.")
-            }
-            else if(u === element.u && p === element.p){
-                console.log(`Login realizado com Sucesso! Bem-vindo ${element.u}`)
-                window.location.assign("app.html")
-            }
+    if(u === '' || p === ''){
+        message.textContent = "Por favor preencha todos os campos."
+        return;
+    }
+    startLoading()
+    const user = result.users.find(element => element.u === u && element.p === p)
+    
+    setTimeout(() => {
+        if(user){
+            message.textContent = `Login realizado com sucesso. Seja, bem-vindo ${user.name}!`
+            message.dataset.success = true
+            document.location.assign('app.html')
         }
-    });
+        else{
+            message.textContent = `O usuário ou senha digitados estão incorretos!`
+            message.dataset.success = false
+            return;
+        }
+    }, 2000)
 }
+
+const startLoading = () =>{
+    if(loading.getAttribute('data-isloading') === 'false'){
+        loading.dataset.isloading = true
+        submit.disabled = true
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keydown', () => {
+        if(loading.getAttribute('data-isloading') === 'true'){
+            loading.dataset.isloading = false
+            submit.disabled = false     
+        }
+    })
+})
+
+submit.addEventListener('click', () => {
+    getData()
+})
