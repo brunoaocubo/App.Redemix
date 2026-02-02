7573c1efd44556545d23b37b79a574996319882d
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-02-02 11:58:08 -0300

    Att: Home Page estrutura da interface completa
    Icones, nomes, seções.

--- js/home.js
+++ js/home.js
@@ -1,28 +1,14 @@
 const section_icon = document.querySelectorAll('.project-section')
-const iconteste = document.querySelector('#principal')
-const card_opt = document.querySelector('#seccondary')
 
-
-iconteste.addEventListener('mouseenter', (evt) =>{ 
-    evt.stopPropagation()
-    card_opt.dataset.isactive = true
-})
-
-iconteste.addEventListener('mouseleave', (evt) =>{ 
-    evt.stopPropagation()
-    card_opt.dataset.isactive = false
-})
-
-/*section_icon.forEach((icon) => {
-    //const project_options = icon.lastElementChild
+section_icon.forEach((icon) => {
+    const project_options = icon.lastElementChild
     icon.addEventListener('mouseenter', (evt) => {
-        console.log(evt.target)
-        card_opt.dataset.isactive = true
+        evt.stopPropagation()
+        project_options.dataset.isactive = true
     })
     icon.addEventListener('mouseleave', (evt) =>{
-        console.log(evt.target)
-        
-        //card_opt.dataset.isactive = false 
+        evt.stopPropagation()
+        project_options.dataset.isactive = false 
     })
-})*/
+})
 
