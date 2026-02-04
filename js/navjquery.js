60f5131b89d738c830fcc80cdb53db82b2209339
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-02-04 08:21:18 -0300

    Fix: Path jquery error 404 githubpages

--- js/navjquery.js
+++ js/navjquery.js
@@ -1,5 +1,10 @@
- document.addEventListener("DOMContentLoaded", function(){
-    $("#nav-placeholder").load("/www/nav.html", function(){
+console.log("Navjquery.js Carregada")
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Evento DOMContentLoaded do Navjquery.js Carregado")
+
+    $("#nav-placeholder").load("../www/nav.html", function(){
+        console.log("Nav.html do Navjquery.js Carregado")
+
         $('.project-section').on({
             mouseenter: function(){
                 $(this).children(0).attr("data-isactive", true)
