66e70a437793db6f3f111219da87bbce33d0913f
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-02-04 15:54:30 -0300

    Att: Estrutura html e css da pagina de assinatura

--- js/navjquery.js
+++ js/navjquery.js
@@ -1,9 +1,5 @@
-console.log("Navjquery.js Carregada")
 document.addEventListener("DOMContentLoaded", function(){
-    console.log("Evento DOMContentLoaded do Navjquery.js Carregado")
-
     $("#nav-placeholder").load("../www/nav.html", function(){
-        console.log("Nav.html do Navjquery.js Carregado")
 
         $('.project-section').on({
             mouseenter: function(){
