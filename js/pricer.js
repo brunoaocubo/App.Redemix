41f3ad1608f4918a4c9208063ffef0ff4f1c2b64
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-02-09 18:09:54 -0300

    Att/fix - Adicionando Status das etiquetas + corrigindo logica tag strong

--- js/pricer.js
+++ js/pricer.js
@@ -1,4 +1,10 @@
 const pricer = document.querySelector('#update-labels')
+const card_price = document.querySelectorAll('.card-eletronic-tag')
+
+card_price.forEach((card) => {
+   //
+    console.log()
+})
 
 pricer.addEventListener('click', (() => {
     confirm('Deseja atualizar todas as etiquetas eletrônicas?')
