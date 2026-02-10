d5737b15f2daff8efdf17d695dfec1e7e6d3e699
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-02-10 18:05:11 -0300

    Att: Template + JS e Json para criar todos os card_pricer

--- js/pricer.js
+++ js/pricer.js
@@ -1,23 +1,160 @@
-const pricer = document.querySelector('#update-labels')
-const card_price = document.querySelectorAll('.card-pricer')
+const btn_update_pricer = document.querySelector('#update-labels')
+const container_cards = document.querySelector('#container-cards')
+const template = document.querySelector('#template')
+let cached_data = null
 
-card_price.forEach((card) => {
-    card.addEventListener('click', () => {
-        let isactive = false
-        let statusCard = card.getAttribute('data-isactive')
+document.addEventListener('DOMContentLoaded', async () => {
+    try{
+        await getData()
+        console.log("Página carregada com sucesso!")
+    }
+    catch(error){
+        console.error("Erro ao carregar", error)
+    }
 
-        if(statusCard == "false")
-        {
-            isactive = true
-        }
-        else{
-            isactive = false
-        }
-
-        card.setAttribute('data-isactive', isactive)
+    const card_pricer = container_cards.querySelectorAll('.card-pricer')
+    card_pricer.forEach((element) =>{
+        element.addEventListener('click', ()=>{
+            showStatus(element)
+        })
     })
 })
 
-pricer.addEventListener('click', (() => {
-    confirm('Deseja atualizar todas as etiquetas eletrônicas?')
-}))
+async function getData(){
+    if(cached_data){
+        //load(cached_data)
+        return;
+    }
+
+    const url = '../ti/pricerstatus.json'
+
+    try {
+        const response = await fetch(url)
+        
+        if(!response.ok){
+            throw new Error(`Response1 status: ${response.status}`)
+        }
+        
+        const statusData = await response.json()
+        cached_data = statusData
+
+        //createCardPricer(statusData, container_cards)
+        loadData(statusData)
+
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+let loadData = (statusData) => {
+    statusData.forEach(data => {
+        updateStatus(data)
+        const clone = template.content.cloneNode(true)
+        let cod_filial = clone.querySelector('#cod')
+        let filial = clone.querySelector('#filial')
+
+        clone.id = data.cod
+        cod_filial.textContent = data.cod
+        filial.textContent = data.filial
+        container_cards.appendChild(clone)
+    }); 
+}
+
+let updateStatus = function(data){
+    let server = data.server_status
+    let archive = data.archive_status
+    let serviceServer = data.services[0]
+    let serviceSQL = data.services[1] 
+
+    const container_status = `
+    <div id="container-status" data-isactive="false">
+        <hr>
+        <section class="status" id="container_status_server">
+            <img src="../imagens/icons/ti_icon.png" alt="" class="icon icon-status">
+            <div class="content">
+                <p class="title">Servidor</p>
+                <p class="label">Nome:
+                    <span class="text-normal" id="server_name">${server.name}</span>
+                </p>
+                <p class="label">Ip:
+                    <span class="text-normal" id="ip">${server.ip}</span>
+                </p>
+                <p class="label">Domínio:
+                    <span class="text-normal" id="domain">${server.domain}</span>
+                </p>
+                <p class="label">Última Atualização:
+                    <span class="text-normal" id="last_update">${server.last_update}</span>
+                </p>
+                <p class="label">Duração:
+                    <span class="text-normal" id="duration_update">${server.duration_update}</span>
+                </p>
+            </div>
+        </section>
+        <hr>
+        <section class="status" id="container_status_archive">
+            <img src="../imagens/icons/ti_icon.png" alt="" class="icon icon-status">
+            <div class="content">
+                <p class="title">Arquivo</p>
+                <p class="label">Nome:
+                    <span class="text-normal" id="archive_name">${archive.name}</span>
+                </p>
+                <p class="label">Tamanho:
+                    <span class="text-normal" id="size">${archive.size}</span>
+                </p>
+                <p class="label">Gerado:
+                    <span class="text-normal" id="created">${archive.created}</span>
+                </p>
+            </div>
+        </section>
+        <hr>
+        <section class="status" id="container_status_services">
+            <img src="../imagens/icons/ti_icon.png" alt="" class="icon icon-status">
+            <div class="content">
+                <p class="title">Serviços</p>
+                <p class="label">Nome:
+                    <span class="text-normal" id="service_name">${serviceServer.name}</span>
+                </p>
+                <p class="label">Status:
+                    <span class="text-normal" id="status">${serviceServer.status}</span>
+                </p>
+                <p class="label">Nome:
+                    <span class="text-normal" id="service_name">${serviceSQL.name}</span>
+                </p>
+                <p class="label">Status:
+                    <span class="text-normal" id="status">${serviceSQL.status}</span>
+                </p>
+            </div>
+        </section>
+    </div>
+    `
+   
+    try {
+        const card_pricer = container_cards.querySelectorAll('.card-pricer')
+
+        card_pricer.forEach((element) => {
+            if(element.id === data.cod){
+                element.insertAdjacentHTML('beforeend', container_status)
+            }            
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+let showStatus = function(card){
+    let isactive = false
+    let statusCard = card.getAttribute('data-isactive')
+    const container_status = card.querySelector('#container-status')
+
+    if(!container_status){
+        console.log('Não encontrou o container_status')
+        return;
+    }
+    statusCard === "false"?isactive = true:isactive = false
+
+    card.setAttribute('data-isactive', isactive)
+    container_status.setAttribute('data-isactive', isactive)
+}
+
+btn_update_pricer.addEventListener('click', (() => {confirm('Deseja atualizar todas as etiquetas eletrônicas?')}))
