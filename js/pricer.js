e64814aebaa46f290822cfdba4e74fa8b6b7b6e3
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-02-10 12:52:42 -0300

    Refatoração Pricer

--- js/pricer.js
+++ js/pricer.js
@@ -1,9 +1,21 @@
 const pricer = document.querySelector('#update-labels')
-const card_price = document.querySelectorAll('.card-eletronic-tag')
+const card_price = document.querySelectorAll('.card-pricer')
 
 card_price.forEach((card) => {
-   //
-    console.log()
+    card.addEventListener('click', () => {
+        let isactive = false
+        let statusCard = card.getAttribute('data-isactive')
+
+        if(statusCard == "false")
+        {
+            isactive = true
+        }
+        else{
+            isactive = false
+        }
+
+        card.setAttribute('data-isactive', isactive)
+    })
 })
 
 pricer.addEventListener('click', (() => {
