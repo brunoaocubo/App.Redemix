ebf6a028e3d735ae59d190d8d403d239abaa1f25
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-02-11 10:04:56 -0300

    Fix Nav Logic

--- js/navjquery.js
+++ js/navjquery.js
@@ -1,28 +1,34 @@
 document.addEventListener("DOMContentLoaded", function(){
     $("#nav-placeholder").load("../www/nav.html", function(){
         const sections = document.querySelectorAll('.project-section')
-        const options = document.querySelectorAll('.projects-options')
-
-        sections.forEach((section) => {
-            section.addEventListener('mouseenter', function(e){
-                e.stopPropagation()
-
-                options.forEach((option) => {
-                    option.setAttribute('data-isactive', false)
-                })
-                
-                const filho = $(this).find('.projects-options')
-                filho.attr('data-isactive', true)       
-            })
-        })
         
-        options.forEach((option) => {
-            option.addEventListener('mouseenter', function(){
-                setTimeout((option.setAttribute('data-isactive', true)), 500)
+        sections.forEach((section) => {
+            const option = section.querySelector('.projects-options')
+            let closerTimer;
+
+            const openMenu = ()=>{
+                clearTimeout(closerTimer)
+                document.querySelectorAll('.projects-options').forEach((opt) => {
+                    if(opt != option){
+                        opt.setAttribute('data-isactive', false)
+                    }
+                })
+                option.setAttribute('data-isactive', true)
+            }
+
+            const startCloserTimer = ()=>{
+                closerTimer = setTimeout(() => {
+                    option.setAttribute('data-isactive', false)
+                }, 500)
+            }
+
+            section.addEventListener('mouseenter', openMenu)
+            section.addEventListener('mouseleave', startCloserTimer)
+            option.addEventListener('mouseenter', () =>{
+                clearTimeout(closerTimer)
             })
-            option.addEventListener('mouseleave', function(){
-                setTimeout(() => {option.setAttribute('data-isactive', false)}, 500)
-            })
+            option.addEventListener('mouseleave', startCloserTimer)
+            
         })
     }) 
 })
