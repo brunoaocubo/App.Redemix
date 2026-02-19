a196768c9dbec7ca2e2461b46e344df312b02bec
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-02-19 17:52:57 -0300

    Vendas Page - Atualizando conteudo do sumário dinamicamente
    Utilizando javascript para atualizar a data do sumário principal nesse primeiro momento, data e dia da semana, mês e ano.

--- js/sale.js
+++ js/sale.js
@@ -1,9 +1,7 @@
 const calendar = document.querySelector('#calendar')
-const date_filter = document.querySelector('.date-filter')
-
-
 
 calendar.addEventListener('click', (() => {
+    const date_filter = document.querySelector('.date-filter')
     if(date_filter.getAttribute('data-isactive') === "false"){
         date_filter.style.display = "flex"
         date_filter.setAttribute('data-isactive', true)
@@ -13,3 +11,34 @@ calendar.addEventListener('click', (() => {
         date_filter.setAttribute('data-isactive', false)
     }
 }))
+
+
+document.addEventListener('DOMContentLoaded', (()=>{
+    updateMainSummary()
+    fixMonth()
+}))
+
+let updateMainSummary = () => {
+    const main_summary = document.querySelector('.main-summary')
+    const year = main_summary.querySelector('.year')
+    const month = main_summary.querySelector('.month')
+    const dayweek = main_summary.querySelector('.dayweek')
+
+   
+    year.textContent = new Date().getFullYear()
+    month.textContent = new Date().getMonth()
+    dayweek.textContent = new Date().getDate()
+}
+
+let fixMonth = () =>{
+    const months = [{0: 'Janeiro'}, {1: 'Fevereiro'}, {2: 'Março'}]
+    const month = new Date().getMonth()
+
+    months.forEach(element => {
+        if(month === Object.keys(months)[element]){
+            
+        }
+    });
+
+    console.log(Object.keys(months)[2])
+}
