6826a505e15906cd42c6f60f4d136a5009a784f3
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-02-20 13:17:26 -0300

    feat: Carregar cupons e exibir valor total formatado em BRL
    - Implementa busca de cupons.json com cache e tratamento de erro.
    - Adiciona lógica de conversão de valores e formatação de moeda.
    - Atualiza sumário de forma assíncrona para exibir o total.

--- js/sale.js
+++ js/sale.js
@@ -1,4 +1,40 @@
 const calendar = document.querySelector('#calendar')
+let cached_data;
+
+document.addEventListener('DOMContentLoaded', async () => {
+    try{
+        await getData()
+        console.log("Página carregada com sucesso!")
+    }
+    catch(error){
+        console.error("Erro ao carregar", error)
+    }
+})
+
+async function getData(){
+    /*
+    if(cached_data){
+        load(cached_data)
+        return;
+    }*/
+
+    const url = '../vendas/cupons.json'
+
+    try {
+        const response = await fetch(url)
+        
+        if(!response.ok){
+            throw new Error(`Response1 status: ${response.status}`)
+        }
+        
+        const cuponsData = await response.json()
+        cached_data = cuponsData
+        
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
 
 calendar.addEventListener('click', (() => {
     const date_filter = document.querySelector('.date-filter')
@@ -36,15 +72,37 @@ let checkDay = (dayWeek) =>{
     return dayWeek;
 }
 
+let getTotalValueCupons = ()=>{
+    if(cached_data === null){
+        console.log('Os dados dos cupons estão vazios')
+        return;
+    }
+
+    let total = 0;
+    let cupons = cached_data
+
+    cupons.forEach((element)=>{
+        const valorLimpo = element.valor.replace(',','.')
+        total += Math.round(parseFloat(valorLimpo * 100))
+    })
+    total = total/100
+    total = total.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+
+    return total
+}
+
 document.addEventListener('DOMContentLoaded', (()=>{
     updateMainSummary()
 }))
 
-let updateMainSummary = () => {
+let updateMainSummary = async () => {
+    await getData()
+
     const main_summary = document.querySelector('.main-summary')
     const s_year = main_summary.querySelector('.year')
     const s_month = main_summary.querySelector('.month')
     const s_dayweek = main_summary.querySelector('.dayweek')
+    const total_value_cupons = main_summary.querySelector('.total-value-cupons')
     
     let dateNow = new Date()
     let year = dateNow.getFullYear()
@@ -55,5 +113,5 @@ let updateMainSummary = () => {
     s_year.textContent = year
     s_month.textContent = checkMonth(month)
     s_dayweek.textContent = `${checkDay(dayweek)}, ${date}`
-    
+    total_value_cupons.textContent = getTotalValueCupons()
 }
