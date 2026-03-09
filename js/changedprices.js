63a0b69f7da263e836987b6b4570dd9d85ae55bc
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-03-09 17:29:46 -0300

    refactor(changedprices): otimiza responsividade e performance dos seletores JS
    - Ajusta layout do .summary e tipografia para dispositivos móveis.
    - Implementa media query (500px) para restaurar larguras e espaçamentos.
    - Otimiza performance do JS realizando o hoisting dos seletores de DOM.
    - Padroniza estilos base de tabela e seletores para telas reduzidas.

--- js/changedprices.js
+++ js/changedprices.js
@@ -1,8 +1,8 @@
 const btn_open_filter = document.querySelector('#open-filter')
+const window_filter = document.querySelector('.window-filter')
+const main = document.querySelector('main')
 
 btn_open_filter.addEventListener('click', ()=>{
-    const window_filter = document.querySelector('.window-filter')
-
     if(window_filter.getAttribute('data-isactive') === 'false'){
         window_filter.setAttribute('data-isactive', true)
     }
